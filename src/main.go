95764c8a1e04cb9b4ee969cef24bc060e350375a
Marco Daillet <marceloidaillet@gmail.com>
2024-04-23 20:30:24 +0200

    feat: styling centralization

--- src/main.go
+++ src/main.go
@@ -1,21 +1,21 @@
 package main
 
 import (
-    "github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-    router := gin.Default()
+	router := gin.Default()
 
-    router.StaticFS("/static", gin.Dir("static", true))
+	router.StaticFS("/static", gin.Dir("src/static", true))
 
-    router.GET("/", showLoginPage)
-    router.GET("/login", showLoginPage)          // Added for login page
-    router.GET("/register", showRegistrationPage) // Added for redirection
-    router.GET("/confirm", confirmationHandler)  // Added for confirmation page
+	router.GET("/", showLoginPage)
+	router.GET("/login", showLoginPage)           // Added for login page
+	router.GET("/register", showRegistrationPage) // Added for redirection
+	router.GET("/confirm", confirmationHandler)   // Added for confirmation page
 
-    router.POST("/register", registerHandler)
-    router.POST("/login", loginHandler) // Added for login form submission
+	router.POST("/register", registerHandler)
+	router.POST("/login", loginHandler) // Added for login form submission
 
-    router.Run(":8080")
+	router.Run(":8080")
 }
